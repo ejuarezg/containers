2b4ce0e8f23113e5d0fb30cbbe6f5bb2e439ddcc
Ezequiel Juarez Garcia <ezequieljuarezg@gmail.com>
2021-05-22 13:21:03 -0400

    Pull upstream changes

--- makepkg/run.sh
+++ makepkg/run.sh
@@ -8,14 +8,16 @@ cd /tmp/pkg
 
 # Install (official repo + AUR) dependencies using yay. We avoid using makepkg
 # -s since it is unable to install AUR dependencies.
-yay -Sy --noconfirm \
-    $(pacman --deptest $(source ./PKGBUILD && echo ${depends[@]} ${makedepends[@]}))
+depends=(); makedepends=(); checkdepends=()
+. ./PKGBUILD
+deps=( "${depends[@]}" "${makedepends[@]}" "${checkdepends[@]}" )
+pacman --deptest "${deps[@]}" | xargs yay -Sy --noconfirm
 
 # Do the actual building
 makepkg -f
 
 # Store the built package(s). Ensure permissions match the original PKGBUILD.
 if [ -n "$EXPORT_PKG" ]; then
-    sudo chown $(stat -c '%u:%g' /pkg/PKGBUILD) ./*.pkg.tar.zst
-    sudo mv ./*.pkg.tar.zst /pkg
+    sudo chown "$(stat -c '%u:%g' /pkg/PKGBUILD)" ./*pkg.tar*
+    sudo mv ./*pkg.tar* /pkg
 fi
